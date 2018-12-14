25c78e1e8d5a6398c7070144b2498b58b21019fa
Suchy1337 <michalsu96@gmail.com>
2018-12-14 21:00:31 +0100

    Dodanie metody obslugujacej przycisk

---
+++ Hurtownia/Assets/Scripts/PrzejscieDoKolejnejSceny.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrzejscieDoKolejnejSceny : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		scene1 = GameObject.Find("Script").GetComponent<EkranGlowny>();
+        scene1.TestCall();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
