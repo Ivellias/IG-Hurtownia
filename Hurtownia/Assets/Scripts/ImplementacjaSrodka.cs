2f3fe16152914d8bf406adfdd19e56f9113ea461
Suchy1337 <michalsu96@gmail.com>
2018-12-14 20:49:53 +0100

    Ustawianie szerokosci oraz wysokosci srodka za pomoca funkcji

---
+++ Hurtownia/Assets/Scripts/ImplementacjaSrodka.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImplementacjaSrodka : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(100f, 50f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
