c6835acf07e921443b66cb031246d510219826c8
Dominik Urban <domi1997gg@gmail.com>
2018-12-15 10:20:20 +0100

    dodanie interfejsu i skryptu
    j/w

---
+++ Hurtownia/Assets/Scripts/SamTekst.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SamTekst : MonoBehaviour, IOnStart {
+
+    public float doStartThingsAndReturnHeightOfThisElement()
+    {
+        int length = gameObject.transform.GetChild(1).gameObject.GetComponent<Text>().text.Length;
+        gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(100, (length / 51 + 1) * 28);
+        return (length / 51 + 1) * 28 + 80;
+    }
+	
+    public void Start()
+    {
+        Debug.Log(doStartThingsAndReturnHeightOfThisElement());
+    }
+
+}
