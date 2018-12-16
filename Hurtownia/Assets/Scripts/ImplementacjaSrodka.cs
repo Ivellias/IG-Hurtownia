f9102e00f7023a49ef59522183e4256d69fbd5b7
Suchy1337 <michalsu96@gmail.com>
2018-12-16 12:01:02 +0100

    Dodano funkcjonalnosc do przyciskow oraz zmieniono wywolanie metod dla tych przyciskow

--- Hurtownia/Assets/Scripts/ImplementacjaSrodka.cs
+++ Hurtownia/Assets/Scripts/ImplementacjaSrodka.cs
@@ -7,18 +7,41 @@ public class ImplementacjaSrodka : MonoBehaviour {
 
 	public GameObject samTekst;
 	private GameObject przykladowyTekst;
+	private GameObject zamowienia;
+	private GameObject faktury;
+	private GameObject opcje;
 	private GameObject oNas;
-	// Use this for initialization
+	private GameObject kontakt;
+
 	void Start () {
-		UsunWszystkiePrefaby();
-		ZainicjalizujPrzykladowyTekst();
+	}
+
+	
+	public void Zamowienia(){
+		zamowienia = UstawPrefab("Zamowienia", "Tekst");
+	}
+
+	public void Faktury(){
+		faktury = UstawPrefab("Faktury", "Tekst");
+	}
+
+	public void Opcje(){
+		opcje = UstawPrefab("Opcje", "Tekst");
 	}
 
 	public void ONas(){
+		oNas = UstawPrefab("O Nas", "Tekst");
+	}
+
+	public void Kontakt(){
+		kontakt = UstawPrefab("Kontakt", "Tekst");
+	}
+
+	private GameObject UstawPrefab(string tytul, string tekst){
 		UsunWszystkiePrefaby();
-		oNas = Instantiate(samTekst, gameObject.transform);
-		oNas.transform.GetChild(0).GetComponent<Text>().text = "O nas";
-		oNas.transform.GetChild(1).GetComponent<Text>().text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor" +
+		GameObject prefab = Instantiate(samTekst, gameObject.transform);
+		prefab.transform.GetChild(0).GetComponent<Text>().text = tytul;
+		prefab.transform.GetChild(1).GetComponent<Text>().text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor" +
 		"incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
 		"commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint" +
 		"occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." + 
@@ -26,22 +49,20 @@ public class ImplementacjaSrodka : MonoBehaviour {
 		"inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo." + 
 		"Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, " + 
 		"sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. ";
-		float wysokosc = oNas.GetComponent<IOnStart>().doStartThingsAndReturnHeightOfThisElement();
+		float wysokosc = prefab.GetComponent<IOnStart>().doStartThingsAndReturnHeightOfThisElement();
 		UstawWysokosc(wysokosc);
+		return prefab;
 	}
 
 	private void UstawWysokosc(float wysokosc){
 		gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(0, wysokosc);
 	}
 
-	private void ZainicjalizujPrzykladowyTekst(){
-		przykladowyTekst = Instantiate(samTekst, gameObject.transform);
-		float wysokosc = przykladowyTekst.GetComponent<IOnStart>().doStartThingsAndReturnHeightOfThisElement();
-		UstawWysokosc(wysokosc);
-	}
-
 	private void UsunWszystkiePrefaby(){
-		GameObject.Destroy(przykladowyTekst, 0f);
+		GameObject.Destroy(zamowienia, 0f);
+		GameObject.Destroy(faktury, 0f);
+		GameObject.Destroy(opcje, 0f);
 		GameObject.Destroy(oNas, 0f);
+		GameObject.Destroy(kontakt, 0f);
 	}
 }
