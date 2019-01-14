89d3b7c95a7081edc441b3f8a7df97809d4c441a
Dominik Urban <domi1997gg@gmail.com>
2019-01-14 20:21:31 +0100

    nowy build niedziala baza
    a

--- Hurtownia/Assets/Scripts/Opcje.cs
+++ Hurtownia/Assets/Scripts/Opcje.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Opcje : MonoBehaviour, IOnStart {
 
@@ -9,6 +10,16 @@ public class Opcje : MonoBehaviour, IOnStart {
     public float doStartThingsAndReturnHeightOfThisElement()
     {
         zmianaHasla.GetComponent<ZmianaHasla>().WyjscieZmianaHasla();
+
+        transform.GetChild(0).GetChild(0).gameObject.GetComponent<Text>().text = "Nazwa: " + OverSceneHandler.aktualnieZalogowanyUzytkownik.Login + " (" + OverSceneHandler.aktualnieZalogowanyUzytkownik.Imie + " " + OverSceneHandler.aktualnieZalogowanyUzytkownik.Nazwisko + ")";
+        transform.GetChild(0).GetChild(1).gameObject.GetComponent<Text>().text = "Adres e-mail: " + OverSceneHandler.aktualnieZalogowanyUzytkownik.Mail;
+        transform.GetChild(0).GetChild(2).gameObject.GetComponent<Text>().text = "Adres: " + OverSceneHandler.aktualnieZalogowanyUzytkownik.Adres;
+        transform.GetChild(0).GetChild(3).gameObject.GetComponent<Text>().text = "NIP: " + OverSceneHandler.aktualnieZalogowanyUzytkownik.NIP;
+        transform.GetChild(0).GetChild(4).gameObject.GetComponent<Text>().text = "REGON: " + OverSceneHandler.aktualnieZalogowanyUzytkownik.REGON;
+        transform.GetChild(0).GetChild(5).gameObject.GetComponent<Text>().text = "KRS: " + OverSceneHandler.aktualnieZalogowanyUzytkownik.KRS; 
+
+
+
         return 400f;
     }
 
